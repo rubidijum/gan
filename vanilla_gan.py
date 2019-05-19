Fix normalization, add intermediate visualisation
Normalisation before applying algorithm now works, the problem was
spelling. Visualise generated images in the process of training a
network. Visualisation images make sense for now, but they are all in
color.

vanilla_gan.py
@@ -8,33 +8,33 @@ from torchvision import transforms, datasets
 from torchsummary import summary
 
 
-batchSize = 200
+batchSize = 100
 
 def real_data_labels(size):
 	data = Variable(torch.ones(size, 1))
 	return data
 	
 def fake_data_labels(size):
-		data = Variable(torch.zeros(size, 1))
-		return data
+	data = Variable(torch.zeros(size, 1))
+	return data
 		
 def train_discriminator(optimizer, real_data, fake_data, discriminator, generator, loss):
 		
-		optimizer.zero_grad()
+	optimizer.zero_grad()
 		
-		prediction_real = discriminator(real_data)
+	prediction_real = discriminator(real_data)
 		
-		error_real = loss(prediction_real, real_data_labels(real_data.size(0)))
-		error_real.backward()
+	error_real = loss(prediction_real, real_data_labels(real_data.size(0)))
+	error_real.backward()
 		
-		prediction_fake = discriminator(fake_data)
+	prediction_fake = discriminator(fake_data)
 		
-		error_fake = loss(prediction_fake, fake_data_labels(fake_data.size(0)))
-		error_fake.backward()
+	error_fake = loss(prediction_fake, fake_data_labels(fake_data.size(0)))
+	error_fake.backward()
 		
-		optimizer.step()
+	optimizer.step()
 		
-		return error_real + error_fake, prediction_real, prediction_fake
+	return error_real + error_fake, prediction_real, prediction_fake
 
 def train_generator(optimizer, fake_data):
 	
@@ -46,6 +46,8 @@ def train_generator(optimizer, fake_data):
 	error = loss(prediction, real_data_labels(prediction.size(0)))
 	error.backward()
 	
+	optimizer.step()
+	
 	return error
 
 
@@ -56,7 +58,7 @@ if __name__ == "__main__":
 	cat_data = utils.CatsDataset("./data/cats", transforms.Compose(
 										[ transforms.Grayscale(num_output_channels=1),
 										  transforms.ToTensor(),
-										  #transforms.Normalize((.5), (.5))										  
+										  transforms.Normalize((.5,), (.5,))										  
 										])
 						  )
 						  
@@ -70,11 +72,8 @@ if __name__ == "__main__":
 	discriminator = networks.DiscriminatorNet()
 	generator = networks.GeneratorNet()
 	
-	#print(discriminator)
-	#print(generator)
-	
-	summary(discriminator, (1,4096))
-	summary(generator, (1,100))
+	#summary(discriminator, (1,4096))
+	#summary(generator, (1,100))
 	
 	d_optimizer = optim.Adam(discriminator.parameters(), lr=0.00002)
 	g_optimizer = optim.Adam(generator.parameters(), lr=0.00002)
@@ -83,63 +82,76 @@ if __name__ == "__main__":
 	
 	# TODO: test me out -> random search	
 	d_steps = 1
-	num_epochs = 2
+	num_epochs = 100
 	
 	num_test_samples = 16
 	test_noise = networks.noise(num_test_samples)
 
 
-logger = utils.Logger(model_name='VGAN', data_name='MNIST')
+	logger = utils.Logger(model_name='VGAN', data_name='MNIST')
 
-discriminator_loss = []
-generator_loss = []
+	discriminator_loss = []
+	generator_loss = []
 
-for epoch in range(num_epochs):
-	for n_batch, (real_batch) in enumerate(data_loader):
+	for epoch in range(num_epochs):
+		for n_batch, real_batch in enumerate(data_loader):
 
-		# 1. Train Discriminator
-		
-		real_data = Variable(networks.images_to_vectors(real_batch))
-		#print(real_data[0].shape)
-		#real_data_show = real_data[0].reshape(3,64,64)
-		#utils.plt.imshow(real_data_show.permute(1,2,0))
-		#utils.plt.show()
-		if torch.cuda.is_available(): real_data = real_data.cuda()
-		# Generate fake data
-		fake_data = generator(networks.noise(real_data.size(0))).detach()
-		#print(fake_data.shape)
-		# Train D
-		d_error, d_pred_real, d_pred_fake = train_discriminator(d_optimizer,
-																real_data, fake_data, discriminator, generator, loss)
-		
-		discriminator_loss.append(d_error)
-
-		# 2. Train Generator
-		# Generate fake data
-		fake_data = generator(networks.noise(real_batch.size(0)))
-		# Train G
-		g_error = train_generator(g_optimizer, fake_data)
-		# Log error
-		logger.log(d_error, g_error, epoch, n_batch, num_batches)
-
-		generator_loss.append(g_error)
-
-		# Display Progress
-		if (n_batch) % 100 == 0:
-			utils.display.clear_output(True)
+			# 1. Train Discriminator
 			
-			utils.plt.plot(discriminator_loss, label = 'D_error')
-			utils.plt.plot(discriminator_loss, label = 'G_error')
-			utils.plt.legend()
-			utils.plt.show()
+			real_data = Variable(networks.images_to_vectors(real_batch))
+			#print(real_data[0].shape)
+			#real_data_show = real_data[0].reshape(3,64,64)
+			#utils.plt.imshow(real_data_show.permute(1,2,0))
+			#utils.plt.show()
+			#if torch.cuda.is_available(): real_data = real_data.cuda()
+			# Generate fake data
+			fake_data = generator(networks.noise(real_data.size(0))).detach()
+			#print(fake_data.shape)
+			# Train D
+			d_error, d_pred_real, d_pred_fake = train_discriminator(d_optimizer,
+																	real_data, fake_data, discriminator, generator, loss)
 			
-			# Display Images
-			test_images = networks.vectors_to_images(generator(test_noise)).data.cpu()
-			logger.log_images(test_images, num_test_samples, epoch, n_batch, num_batches);
-			# Display status Logs
-			logger.display_status(
-				epoch, num_epochs, n_batch, num_batches,
-				d_error, g_error, d_pred_real, d_pred_fake
-			)
-		# Model Checkpoints
-		logger.save_models(generator, discriminator, epoch)
+			discriminator_loss.append(d_error)
+
+			# 2. Train Generator
+			# Generate fake data
+			fake_data = generator(networks.noise(real_batch.size(0)))
+			# Train G
+			g_error = train_generator(g_optimizer, fake_data)
+			# Log error
+			logger.log(d_error, g_error, epoch, n_batch, num_batches)
+
+			generator_loss.append(g_error)
+
+			# Display Progress
+			if (n_batch) % 50 == 0 or (n_batch) < 50:
+				utils.display.clear_output(True)
+				
+				utils.plt.plot(discriminator_loss, label = 'D_error')
+				utils.plt.plot(discriminator_loss, label = 'G_error')
+				utils.plt.legend()
+				utils.plt.show()
+				
+				fig = utils.plt.figure(figsize=(8,2))
+				cols = 8
+				rows = 2
+				
+				
+				# Display Images
+				test_images = networks.vectors_to_images(generator(test_noise)).data.cpu()
+				
+				for i in range(1, cols*rows):
+					img = test_images[i]
+					fig.add_subplot(rows, cols, i)
+					utils.plt.imshow(img.permute(1,2,0).squeeze(), cmap='gray')
+				utils.plt.show()
+					
+				
+				logger.log_images(test_images, num_test_samples, epoch, n_batch, num_batches);
+				# Display status Logs
+				logger.display_status(
+					epoch, num_epochs, n_batch, num_batches,
+					d_error, g_error, d_pred_real, d_pred_fake
+				)
+			# Model Checkpoints
+			logger.save_models(generator, discriminator, epoch)