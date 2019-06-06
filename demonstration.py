e40788d71369125d5de746d4a428bc1663b13aa4
Aleksandar Vracarevic <vracarevicaleksandar@gmail.com>
2019-06-06 23:16:12 +0200

    Adjust demonstration script
    Add skeleton for DCGAN model demostration.

--- demonstration.py
+++ demonstration.py
@@ -10,9 +10,7 @@ if __name__ == "__main__":
 	
 	l = utils.Logger("demonstrate", "demonstrate")
 	
-	
-	
-	architectures = ['VGAN', 'GAN_improve']
+	architectures = ['VGAN', 'GAN_improve', 'dummy', 'DCGAN']
 	
 	data = ['Cats', 'Cars']
 	
@@ -20,7 +18,7 @@ if __name__ == "__main__":
 				[1] Vanilla GAN
 				[2] Improved GAN (one-sided label smoothing, minibatch discrimination layer)
 				[3] Unrolled GAN (not yet implemented)
-				[4] DCGAN (not yet implemented)
+				[4] DCGAN
 			""")
 	
 	arch = architectures[int(input())-1]
@@ -36,6 +34,10 @@ if __name__ == "__main__":
 		G = networks.GeneratorNet()
 	elif (arch == "GAN_improve"):
 		G = networks_improve.GeneratorNet()
+	elif(arch == "DCGAN"):
+		pass; # TODO: implement me later
+	
+	
 	
 	for _,_,fnames in os.walk("./data/models/{}".format(arch)):
 		model_list = fnames #sorted(fnames, reverse = True)
@@ -63,27 +65,32 @@ if __name__ == "__main__":
 		print("Iteration number " + str(itr))
 		
 		if(arch == "GAN_improve"):
+			
 			test_noise = networks_improve.noise(num_samples)
 			fake_data = G(test_noise).detach()
 			fake_imgs = networks_improve.vectors_to_images(fake_data)
 		elif(arch == "VGAN"):
+			
 			test_noise = networks.noise(num_samples)
 			fake_data = G(test_noise).detach()
 			fake_imgs = networks.vectors_to_images(fake_data)
+		elif(arch == "DCGAN"):
+			pass;
 		else:
 			print("ERROR!");
+			exit();
 		
 		fig = utils.plt.figure(figsize=(5,5))
 		
 		for i in range(1, rows*cols+1):
-			print(i)
+			
 			img = fake_imgs[i-1]
 			fig.add_subplot(rows, cols, i, xticks=[], yticks=[])
-			print(img.size())
-			if(arch == "GAN_IMPROVE"):
+			if(arch == "GAN_improve"):
+				
 				utils.plt.imshow(img.permute(1,2,0))
 			elif(arch == "VGAN"):
-				utils.plt.imshow(img.permute(1,2,0).squeeze(), cmap='gray')
+				utils.plt.imshow(img.permute(1,2,0).squeeze(), cmap='gray') #TODO: adjust this for color 
 		utils.plt.axis('off')
 		utils.plt.show()
 		
